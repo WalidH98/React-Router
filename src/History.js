164a1ad37c8ea3fb8549e43e87061e8c25861332
walid <walid.hamiciwild@gmail.com>
2019-04-09 10:29:49 +0200

    reactRouter

---
+++ src/History.js
@@ -0,0 +1,14 @@
+
+import React, { Component } from 'react';
+
+class Hystory extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Notre histoire</h1>
+            </div>
+        );
+    }
+}
+
+export default Hystory;
---
+++ src/Product.js
@@ -0,0 +1,14 @@
+
+import React, { Component } from 'react';
+
+class Product extends Component {
+    render() {
+        return (
+            <div>
+                {this.props.match.params.productNumber}
+            </div>
+        );
+    }
+}
+
+export default Product;
